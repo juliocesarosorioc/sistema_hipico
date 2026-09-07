7f8fae5114efedfa0dc1054c7d20657f4132a23e
Julio <juliocesarosorioc@gmail.com>
2026-09-07 17:57:48 -0400

    Aplica llave anon correcta para autorizar conexion

--- js/conexion.js
+++ js/conexion.js
@@ -1,10 +1,10 @@
 const SUPABASE_URL = 'https://dbkvnqzchtcabhiaqdyi.supabase.co'; 
-const SUPABASE_ANON_KEY = 'sb_publishable_hEazSQ2Tp3YLDUoUINIIPw_yuduDLWi';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRia3ZucXpjaHRjYWJoaWFxZHlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzNTkxOTYsImV4cCI6MjEwMzkzNTE5Nn0._ciSMFx_7Ogrt9XAy9YJzJTb2FKYWSwxf_0ryvgU35E';
 
 try {
     if (typeof window.supabase !== 'undefined') {
         window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-        console.log("✅ Conexión con Supabase configurada.");
+        console.log("✅ Conexión con Supabase configurada exitosamente.");
     } else {
         console.error("Error: La librería de Supabase no cargó en el HTML.");
     }
