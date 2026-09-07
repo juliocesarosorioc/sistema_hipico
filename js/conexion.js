1b9f29d452aafbeaea5aa187d2a085550486ea28
Julio <juliocesarosorioc@gmail.com>
2026-09-07 17:54:04 -0400

    Inserta llave publica para resolver conexion

--- js/conexion.js
+++ js/conexion.js
@@ -1,10 +1,6 @@
-// Archivo: js/conexion.js
-// Propósito: Inicializar la conexión con Supabase
-
 const SUPABASE_URL = 'https://dbkvnqzchtcabhiaqdyi.supabase.co'; 
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRia3ZucXpjaHRjYWJoaWFxZHlpIipiIjAsImlhdCI6MTc4Zjg3NTE5Niwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzNTkxOTYsImV4cCI6MjEwMzkzNTE5Nn0._ciSMFx_7Ogrt9XAy9YJzJTb2FKYWSwxf_0ryvgU35E';
+const SUPABASE_ANON_KEY = 'sb_publishable_hEazSQ2Tp3YLDUoUINIIPw_yuduDLWi';
 
-// Se reasigna el cliente a la variable global evitando colisiones
 try {
     if (typeof window.supabase !== 'undefined') {
         window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
